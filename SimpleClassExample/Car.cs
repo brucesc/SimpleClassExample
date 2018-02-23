7c9d184eb6c9ebe46b64d7d809ea63ab08bd2886
Steven Bruce <Brucesc726@gmail.com>
2018-02-23 11:24:03 -0500

    initial

---
+++ SimpleClassExample/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        // The 'state' of the Car.
+        public string petName;
+        public int currSpeed;
+
+        // The functionality of the Car.
+        // Using the expression-bodied member syntax introduced in C# 6 
+        public void PrintState()
+            => Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+
+        public void SpeedUp(int delta)
+            => currSpeed += delta;
+       
+        // we learned that we can overload a class and supposedly a method, but we can also overload a constructor
+        // a constructor definition is on page 165, but here is an example of one
+        public Car()
+        {
+            petName = "Chuck";
+            currSpeed = 10;
+        }
+        public Car(string pn)
+        {
+            petName = pn;
+        }
+        public Car(string pn, int cs)
+        {
+            petName = pn;
+            currSpeed = cs;
+        }
+        
+        // The snippet ctor tab tab can be used to quickly create a custom constructor
+    }
+    
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ SimpleClassExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun With Class Types ******\n");
+            // Allocate and configure a Car object.
+            Car myCar = new Car //this could also be Car myCar = new Car(); and the properties could be set by myCar.currSpeed = 10; myCar.petName = "Henry";
+            {
+                currSpeed = 10,
+                petName = "Henry"
+            };
+            for (int i = 0; i <= 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+            
+            Car Chuck = new Car();
+            Chuck.PrintState(); // here we see the new constructor already set the values of the Car class properties to a new default
+
+            Car Mary = new Car("Mary");
+            Mary.PrintState();
+
+            Car Daisy = new Car("Daisy", 75);
+            Daisy.PrintState();
+
+            // we just used the new overloaded constructor to create three cars. It was much quicker to initialize a new instance of Car and fill out field data
+
+            Console.ReadLine();
+        }
+    }
+}
